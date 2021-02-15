3179aac74135598e2753363599d155b625d85049
Adam Israel <adam@adamisrael.com>
2021-02-14 20:46:38 -0500

    Add -anniversary flag
    Start to make the `gedcom` command smarter, with the addition of a
    `-anniversary` flag that will print out individuals who have an event
    falling on the current day (BIRT right now)

--- cmd/gedcom/main.go
+++ cmd/gedcom/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
 
 	"github.com/adamisrael/gedcom"
 	// "gedcom"
+	"github.com/araddon/dateparse"
 )
+
+var dateFormats = []string{
+	"12 Feb 2006",
+	"03 February 2013",
+}
 
 func check(e error) {
 	if e != nil {
@@ -19,6 +26,7 @@ func main() {
 	// verbose := flag.Bool("verbose", false, "verbosity")
 	gedcomFile := flag.String("gedcom", "", "The path to the GEDCOM file to analyze.")
 	format := flag.String("format", "", "The format to output in (text, json).")
+	anniversary := flag.Bool("anniversary", false, "Print all individuals who have an anniverary on this date.")
 
 	flag.Parse()
 
@@ -45,13 +53,63 @@ func main() {
 					fmt.Printf("Name: %q\n", n.Name)
 				}
 
-				for _, n := range i.Event {
-					fmt.Printf("Event: %q\n", n.Date)
-				}
+				// for _, n := range i.Event {
+				// 	fmt.Printf("Event: %q\n", n.Date)
+				// }
 			}
 
-			fmt.Printf("%+v\n", i)
-			fmt.Printf("%#v\n", i)
+			// fmt.Printf("%+v\n", i)
+			// fmt.Printf("%#v\n", i)
+		}
+
+		if *anniversary {
+
+			currentTime := time.Now()
+
+			for _, event := range i.Event {
+				if len(event.Date) > 0 {
+					t, err := dateparse.ParseLocal(event.Date)
+					if err == nil {
+						year, month, day := dateDiff(t, currentTime)
+						if month == 0 && day == 0 {
+							switch event.Tag {
+							case "BIRT":
+								fmt.Printf("%s was born on this date %d years ago\n", i.Name[0].Name, year)
+							}
+
+						}
+					}
+				}
+			}
 		}
 	}
+}
+
+func dateDiff(a, b time.Time) (year, month, day int) {
+	if a.Location() != b.Location() {
+		b = b.In(a.Location())
+	}
+	if a.After(b) {
+		a, b = b, a
+	}
+	y1, M1, d1 := a.Date()
+	y2, M2, d2 := b.Date()
+
+	year = int(y2 - y1)
+	month = int(M2 - M1)
+	day = int(d2 - d1)
+
+	// Normalize negative values
+	if day < 0 {
+		// days in month:
+		t := time.Date(y1, M1, 32, 0, 0, 0, 0, time.UTC)
+		day += 32 - t.Day()
+		month--
+	}
+	if month < 0 {
+		month += 12
+		year--
+	}
+
+	return
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/adamisrael/gedcom
 
 go 1.15
+
+require github.com/araddon/dateparse v0.0.0-20210207001429-0eec95c9db7e
